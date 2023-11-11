cf6885d8140605ac5d8dfedc58773895cbf7e648
ErrorNoInternet <errornointernet@envs.net>
2023-11-12 02:59:58 +0800

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "neofetch in discord";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in rec
+      {
+        packages.rpcfetch = pkgs.stdenv.mkDerivation {
+          pname = "rpcfetch";
+          version = "0.1.0";
+          src = ./.;
+
+          buildInputs = with pkgs; [
+            curl
+            cjson
+            gcc
+            pcre2
+            unzip
+            wget
+            xorg.libX11.dev
+          ];
+
+          buildPhase = ''
+            cp -r ${pkgs.discord-gamesdk}/lib .
+            cp -r ${pkgs.discord-gamesdk.dev}/lib/include .
+            make offline
+          '';
+
+          makeFlags = with pkgs; [
+            "PREFIX=$(out)"
+          ];
+        };
+        defaultPackage = packages.rpcfetch;
+      }
+    ));
+}
